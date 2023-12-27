6d29bee531b91a92c015c483b59609bab78c48ad
Bill Harding <wbharding@gmail.com>
2023-12-27 13:13:57 -0800

    Modernize plugin-template

--- esbuild.js
+++ esbuild.js
@@ -3,17 +3,13 @@ import esbuild from "esbuild"
 
 dotenv.config();
 
-// Taken from internet recommendation on getting an esbuild https://medium.com/geekculture/build-a-library-with-esbuild-23235712f3c
-// Will we actually need to build this? TBD
-esbuild
-    .build({
-        entryPoints: ["src/index.js"],
-        outdir: "lib",
-        bundle: true,
-        sourcemap: true,
-        minify: true,
-        splitting: true,
-        format: "esm",
-        target: ["esnext"]
-    })
-    .catch(() => process.exit(1));
+const result = await esbuild.build({
+  entryPoints: [`lib/plugin.js`],
+  bundle: true,
+  format: "iife",
+  outfile: "build/compiled.js",
+  packages: "external",
+  platform: "node",
+  write: true,
+});
+console.log("Result was", result)
